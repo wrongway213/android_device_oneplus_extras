0a2527f675209dde0b43b360d8f903b023015b14
Alex Cruz <du.alexcruz@gmail.com>
2017-05-21 22:51:19 +0200

    Further Refactoring, change theme to match pixel (settings)
    - Remove package from showing up in settings
    
    - Changed package name so we can do our own thing and not have it
    get in the way of the Doze package currently used by Nick and others
    
    - Set OneplusDoze to compile
    
    Change-Id: Ieebf13f88c02d5036a09b5ef83ca1b53bd6999ac

--- common.mk
+++ common.mk
@@ -20,6 +20,7 @@ DEVICE_PACKAGE_OVERLAYS += $(LOCAL_PATH)/overlay
 # Keyhandler
 PRODUCT_PACKAGES += \
     ConfigPanel \
+    OneplusDoze \
     com.cyanogenmod.keyhandler
 
 PRODUCT_SYSTEM_SERVER_JARS += com.cyanogenmod.keyhandler
